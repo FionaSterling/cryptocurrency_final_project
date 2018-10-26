55d8c92e6e7d5bbde373d47e0ac776152677e61f
Fiona Sterling <fsterling@hchc.edu>
2018-10-26 14:40:33 -0400

    Importing config properties properly

--- config.py
+++ config.py
@@ -3,3 +3,4 @@ import os
 class Config(object):
     API_KEY = os.environ.get('API_KEY') or 'you-will-never-guess'
     NEWS_API_KEY = os.environ.get('NEWS_API_KEY')
+    SECRET_KEY = os.environ.get('SECRET_KEY') or 'you-will-never-guess'
